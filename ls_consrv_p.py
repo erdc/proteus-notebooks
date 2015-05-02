9d70d63841eca436be8b8a9005e69816469de464
cekees <cekees@gmail.com>
2015-05-02 17:43:54 -0500

    updated to IPython 3.1.0, some are  still  broken

--- ls_consrv_p.py
+++ ls_consrv_p.py
@@ -1,12 +1,12 @@
 from proteus import *
 from proteus.default_p import *
-from wavetank import *
+from tank import *
 from proteus.mprans import MCorr
 
 LevelModelType = MCorr.LevelModel
 
-coefficients = MCorr.Coefficients(LSModel_index=1,V_model=0,me_model=4,VOFModel_index=2,
-                                  applyCorrection=True,nd=nd,checkMass=False,useMetrics=useMetrics,
+coefficients = MCorr.Coefficients(LSModel_index=2,V_model=0,me_model=4,VOFModel_index=1,
+                                  applyCorrection=applyCorrection,nd=nd,checkMass=False,useMetrics=useMetrics,
                                   epsFactHeaviside=epsFact_consrv_heaviside,
                                   epsFactDirac=epsFact_consrv_dirac,
                                   epsFactDiffusion=epsFact_consrv_diffusion)
